fb8db81e1e8d6fa7e55fd64e553c5f446e1c3d63
Chris Philbin <chris@chrisphilbin.net>
2022-03-12 17:35:02 -0500

    Adding models

---
+++ frontend/src/models/employee-model.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  birthdate: string;
+  createdAt: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  interests: string[];
+  lastInteraction: string;
+  sportsTeams: string[];
+  userId: string;
+}
---
+++ frontend/src/models/meeting-model.ts
@@ -0,0 +1,6 @@
+export interface Meeting {
+  createdAt: string;
+  meetingDate: string;
+  notes: string;
+  userId: string;
+}
