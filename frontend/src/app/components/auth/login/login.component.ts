a5a8900078ca542c6e4de3980688cb597b15808f
Chris Philbin <chris@chrisphilbin.net>
2022-03-13 09:54:50 -0400

    Adding tailwindcss and creating auth component/service

---
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthResponseData, AuthService } from '../../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [],
+})
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  isLoginMode = true;
+  isLoading = false;
+  error: string = '';
+
+  ngOnInit(): void {
+    // console.log(this.authError)
+    // this.authService.errorMessage.subscribe((error) => {
+    //   this.authError = error
+    //   console.log(this.authError, "auth error")
+    // })
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const { username, email, password, confirmPassword } = form.value;
+
+    if (!this.isLoginMode && password !== confirmPassword) {
+      this.error = 'Passwords do not match.';
+      return;
+    }
+
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
+    if (this.isLoginMode) {
+      authObs = this.authService.login(email, password);
+    } else {
+      authObs = this.authService.signup(email, password);
+    }
+
+    authObs.subscribe(
+      (response) => {
+        this.isLoading = false;
+        this.authService.user.subscribe((user) => {
+          console.log(user);
+        });
+        this.router.navigate(['/']);
+      },
+      (errorMessage) => {
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+
+    form.reset();
+  }
+}
---
+++ frontend/src/app/services/auth.service.ts
@@ -0,0 +1,101 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, tap } from 'rxjs/operators';
+import { BehaviorSubject, Subject, throwError } from 'rxjs';
+import { User } from '../../models/user-model';
+
+export interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  user = new BehaviorSubject<User | null>(null);
+  errorMessage = new Subject();
+
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBhJGSfs_u0THw9gg1q-4CH9ohcyy6PUco',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) => {
+          this.handleAuthentication(
+            responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn
+          );
+        })
+      );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBhJGSfs_u0THw9gg1q-4CH9ohcyy6PUco',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((responseData) => {
+          this.handleAuthentication(
+            responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+    localStorage.setItem('AuthToken', `Bearer ${token}`);
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured!';
+
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorResponse.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email has already been taken';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'Email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Password not valid';
+        break;
+    }
+    return throwError(errorMessage);
+  }
+}
