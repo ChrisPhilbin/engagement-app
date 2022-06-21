df111f4f6186348b066d6b9bdd1de0bf1db977f8
Chris Philbin <chrisp@runway.com.au>
2022-06-21 08:48:09 -0400

    Updating auth interceptor service to clear session cookies and redirect user to login page if they are not logged in or session has expired

--- frontend/src/app/services/auth-interceptor.service.ts
+++ frontend/src/app/services/auth-interceptor.service.ts
@@ -9,16 +9,19 @@ import {
 } from '@angular/common/http';
 import { exhaustMap, take, Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptorService implements HttpInterceptor {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return this.authService.user.pipe(
       take(1),
       exhaustMap((user) => {
         if (!user) {
+          this.authService.logout();
+          this.router.navigate(['/login']);
           return next.handle(req);
         }
         const modifiedRequest = req.clone({
