b6024928b73d6de24e3d4dc167a14ed009593186
Chris Philbin <chris@chrisphilbin.net>
2022-07-25 18:38:49 -0400

    Configuring environments and firebase

---
+++ frontend/src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  appName: 'DEV -- Engage -',
+  firebaseApiUrl: 'http://localhost:5001/ng-blog-574e0/us-central1/engagement',
+};
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  appName: 'Engage -',
+  // firebaseApiUrl: 'http://localhost:5001/ng-blog-574e0/us-central1/engagement',
+  firebaseApiUrl:
+    'https://us-central1-ng-blog-574e0.cloudfunctions.net/engagement',
 };
