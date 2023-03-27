1a690dbf58177b1b96471f01955162b030188547
Chris Philbin <chrisp@runway.com.au>
2023-03-27 17:47:28 -0400

    Converting settings.js to typescript

--- functions/api/settings.ts
+++ functions/api/settings.ts
@@ -1,7 +1,8 @@
-const { db } = require("../util/admin");
-const { defaultAppSettings } = require("../util/settingsHelper");
+import { Response } from "express";
+import { db } from "../util/admin";
+import { IAppRequest } from "./models/app-request-model";
 
-exports.saveSettings = (request, response) => {
+export const saveSettings = (request: IAppRequest, response: Response) => {
   db.collection("users")
     .where("userId", "==", request.user.uid)
     .limit(1)
@@ -15,26 +16,17 @@ exports.saveSettings = (request, response) => {
     });
 };
 
-exports.getSettings = async (request, response) => {
+export const getSettings = async (request: IAppRequest, response: Response) => {
   try {
     const userFromDb = await db.collection("users").where("userId", "==", request.user.uid).limit(1).get();
     if (userFromDb) {
       return response.status(200).json(userFromDb.docs[0].data().appSettings);
     } else {
       return response.status(404).json({ error: "User not found." });
-      //user doc doesn't exist - create new user with default settings and return those setitngs
-      //   const newUser = {
-      //     userId: request.user.uid,
-      //     email: request.user.email,
-      //     createdAt: new Date(),
-      //     appSettings: request.body.appSettings,
-      //   };
-      //   const newUserDoc = await db.collection("users").add(newUser);
-      //   return response.status(200).json(newUserDoc);
     }
   } catch (error) {
     console.log(error, "Something went wrong.");
   }
 };
 
-exports.updateSettings = (request, response) => {};
+// exports.updateSettings = (request, response) => {};
--- functions/lib/api/settings.js
+++ functions/lib/api/settings.js
@@ -1,8 +1,9 @@
 "use strict";
-const { db } = require("../util/admin");
-const { defaultAppSettings } = require("../util/settingsHelper");
-exports.saveSettings = (request, response) => {
-    db.collection("users")
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getSettings = exports.saveSettings = void 0;
+const admin_1 = require("../util/admin");
+const saveSettings = (request, response) => {
+    admin_1.db.collection("users")
         .where("userId", "==", request.user.uid)
         .limit(1)
         .get()
@@ -14,28 +15,21 @@ exports.saveSettings = (request, response) => {
         return response.status(200).json(tempUser.appSettings);
     });
 };
-exports.getSettings = async (request, response) => {
+exports.saveSettings = saveSettings;
+const getSettings = async (request, response) => {
     try {
-        const userFromDb = await db.collection("users").where("userId", "==", request.user.uid).limit(1).get();
+        const userFromDb = await admin_1.db.collection("users").where("userId", "==", request.user.uid).limit(1).get();
         if (userFromDb) {
             return response.status(200).json(userFromDb.docs[0].data().appSettings);
         }
         else {
             return response.status(404).json({ error: "User not found." });
-            //user doc doesn't exist - create new user with default settings and return those setitngs
-            //   const newUser = {
-            //     userId: request.user.uid,
-            //     email: request.user.email,
-            //     createdAt: new Date(),
-            //     appSettings: request.body.appSettings,
-            //   };
-            //   const newUserDoc = await db.collection("users").add(newUser);
-            //   return response.status(200).json(newUserDoc);
         }
     }
     catch (error) {
         console.log(error, "Something went wrong.");
     }
 };
-exports.updateSettings = (request, response) => { };
+exports.getSettings = getSettings;
+// exports.updateSettings = (request, response) => {};
 //# sourceMappingURL=settings.js.map
