c08464c7a9f431e7c83648e701adc2c9f7c6f382
Chris Philbin <chrisp@runway.com.au>
2023-03-27 21:57:37 -0400

    Converting meetings.js to typescript

--- functions/lib/api/meetings.js
+++ functions/lib/api/meetings.js
@@ -1,16 +1,19 @@
 "use strict";
-const { db } = require("../util/admin");
-exports.createMeeting = (request, response) => {
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteMeeting = exports.updatedMeetingDetails = exports.getMeetingDetails = exports.getAllMeetingsForEmployee = exports.createMeeting = void 0;
+const admin_1 = require("../util/admin");
+const createMeeting = (request, response) => {
     if (request.body.notes.trim() === "") {
         return response.status(400).json({ error: "Meeting notes cannot be blank" });
     }
-    db.doc(`/employees/${request.params.employeeId}`)
+    admin_1.db.doc(`/employees/${request.params.employeeId}`)
         .get()
         .then((doc) => {
+        var _a;
         if (!doc.exists) {
             return response.status(404).json();
         }
-        if (doc.data().userId !== request.user.uid) {
+        if (((_a = doc.data()) === null || _a === void 0 ? void 0 : _a.userId) !== request.user.uid) {
             return response.status(401).json({ error: "You are not authorized." });
         }
     });
@@ -22,7 +25,7 @@ exports.createMeeting = (request, response) => {
         agreedUponActions: request.body.agreedUponActions,
         hasOutstandingActionItems: request.body.hasOutstandingActionItems,
     };
-    db.collection(`employees/${request.params.employeeId}/meetings`)
+    admin_1.db.collection(`employees/${request.params.employeeId}/meetings`)
         .add(newMeeting)
         .then((doc) => {
         const responseMeeting = newMeeting;
@@ -33,18 +36,20 @@ exports.createMeeting = (request, response) => {
         response.status(500).json({ error: "Something went wrong." });
     });
 };
-exports.getAllMeetingsForEmployee = (request, response) => {
-    db.doc(`/employees/${request.params.employeeId}`)
+exports.createMeeting = createMeeting;
+const getAllMeetingsForEmployee = (request, response) => {
+    admin_1.db.doc(`/employees/${request.params.employeeId}`)
         .get()
         .then((doc) => {
+        var _a;
         if (!doc.exists) {
             return response.status(404).json();
         }
-        if (doc.data().userId !== request.user.uid) {
+        if (((_a = doc.data()) === null || _a === void 0 ? void 0 : _a.userId) !== request.user.uid) {
             return response.status(401).json({ error: "You are not authorized." });
         }
     });
-    db.collection(`/employees/${request.params.employeeId}/meetings`)
+    admin_1.db.collection(`/employees/${request.params.employeeId}/meetings`)
         .orderBy("createdAt", "asc")
         .get()
         .then((data) => {
@@ -61,34 +66,39 @@ exports.getAllMeetingsForEmployee = (request, response) => {
         return response.status(500).json({ error: "Something went wrong." });
     });
 };
-exports.getMeetingDetails = (request, response) => {
-    db.doc(`/employees/${request.params.employeeId}`)
+exports.getAllMeetingsForEmployee = getAllMeetingsForEmployee;
+const getMeetingDetails = (request, response) => {
+    admin_1.db.doc(`/employees/${request.params.employeeId}`)
         .get()
         .then((doc) => {
+        var _a;
         if (!doc.exists) {
             return response.status(404).json();
         }
-        if (doc.data().userId !== request.user.uid) {
+        if (((_a = doc.data()) === null || _a === void 0 ? void 0 : _a.userId) !== request.user.uid) {
             return response.status(401).json({ error: "You are not authorized." });
         }
     });
-    db.doc(`employees/${request.params.employeeId}/meetings/${request.params.meetingId}`)
+    admin_1.db.doc(`employees/${request.params.employeeId}/meetings/${request.params.meetingId}`)
         .get()
         .then((doc) => {
-        if (doc.data().userId !== request.user.uid) {
+        var _a;
+        if (((_a = doc.data()) === null || _a === void 0 ? void 0 : _a.userId) !== request.user.uid) {
             return response.status(401).json({ error: "You are not authorized" });
         }
         return response.status(200).json(Object.assign(Object.assign({}, doc.data()), { meetingId: doc.id }));
     });
 };
-exports.updatedMeetingDetails = (request, response) => {
-    db.doc(`/employees/${request.params.employeeId}`)
+exports.getMeetingDetails = getMeetingDetails;
+const updatedMeetingDetails = (request, response) => {
+    admin_1.db.doc(`/employees/${request.params.employeeId}`)
         .get()
         .then((doc) => {
+        var _a;
         if (!doc.exists) {
             return response.status(404).json();
         }
-        if (doc.data().userId !== request.user.uid) {
+        if (((_a = doc.data()) === null || _a === void 0 ? void 0 : _a.userId) !== request.user.uid) {
             return response.status(401).json({ error: "You are not authorized." });
         }
     });
@@ -98,7 +108,7 @@ exports.updatedMeetingDetails = (request, response) => {
     if (request.body.notes.trim() === "") {
         return response.status(403).json({ error: "Field cannot be blank." });
     }
-    let document = db.doc(`employees/${request.params.employeeId}/meetings/${request.params.meetingId}`);
+    let document = admin_1.db.doc(`employees/${request.params.employeeId}/meetings/${request.params.meetingId}`);
     document
         .update(request.body)
         .then((updatedMeeting) => {
@@ -108,19 +118,21 @@ exports.updatedMeetingDetails = (request, response) => {
         return response.status(500).json({ error: "Something went wrong." });
     });
 };
-exports.deleteMeeting = (request, response) => {
+exports.updatedMeetingDetails = updatedMeetingDetails;
+const deleteMeeting = (request, response) => {
     console.log("deleting meeting...");
-    db.doc(`/employees/${request.params.employeeId}`)
+    admin_1.db.doc(`/employees/${request.params.employeeId}`)
         .get()
         .then((doc) => {
+        var _a;
         if (!doc.exists) {
             return response.status(404).json();
         }
-        if (doc.data().userId !== request.user.uid) {
+        if (((_a = doc.data()) === null || _a === void 0 ? void 0 : _a.userId) !== request.user.uid) {
             return response.status(401).json({ error: "You are not authorized." });
         }
     });
-    db.collection(`employees/${request.params.employeeId}/meetings`)
+    admin_1.db.collection(`employees/${request.params.employeeId}/meetings`)
         .doc(request.params.meetingId)
         .delete()
         .then((doc) => {
@@ -130,4 +142,5 @@ exports.deleteMeeting = (request, response) => {
         response.status(500).json({ error: "Something went wrong." });
     });
 };
+exports.deleteMeeting = deleteMeeting;
 //# sourceMappingURL=meetings.js.map
