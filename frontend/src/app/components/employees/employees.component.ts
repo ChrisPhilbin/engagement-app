b2d5de30c19ff76ac3c9ff098fc3357394995dd5
Chris Philbin <chrisp@runway.com.au>
2023-05-12 14:44:55 -0400

    Adding helper message if the user has not created any employees

--- frontend/src/app/components/employees/employees.component.ts
+++ frontend/src/app/components/employees/employees.component.ts
@@ -18,7 +18,6 @@ export class EmployeesComponent implements OnInit {
   filterByBirthday: boolean = false;
   filterByOverdueInteractions: boolean = false;
   filterByUpcomingAnniversaries: boolean = false;
-  showGridLayout: boolean = false;
 
   constructor(
     public employeeService: EmployeeService,
@@ -109,9 +108,5 @@ export class EmployeesComponent implements OnInit {
     if (confirm('Are you sure you want to remove this employee?')) {
       this.employeeService.deleteEmployeeById(employeeId);
     }
-  }
-
-  toggleGridLayout() {
-    this.showGridLayout = !this.showGridLayout;
   }
 }
